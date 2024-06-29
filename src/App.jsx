f83e89d086e19f8fa78f76518e3f0c7be2e4fd03
Methmini Madhushika <130038736+methminimadhush2000@users.noreply.github.com>
2024-06-29 09:15:07 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
+//App.jsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {CssBaseline} from "@mui/material";
+import { Box } from "@mui/system";
+//import Schedules from "./pages/Schedules";
+//import TrainDetails from "./pages/TrainDetails";
+//import Login from "./pages/Login";
+//import Register from "./pages/Register";
+//import SearchBar from "./components/SearchBar";
+import Home from "./pages/Home";
+import PassengerDetails from "./pages/PassengerDetails";
 
-import Home from "./Home"
 function App() {
-  
-
   return (
-    <>
-      <h1>TrainEase</h1>
-      
-    </>
-  )
+    <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+      <CssBaseline />
+      <BrowserRouter>
+        {/* <Header /> */}
+        <Box sx={{ flex: "1 0 auto", backgroundColor: "#f5f5f5" }}>
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            {/* <Route path="/schedules" element={<Schedules />} /> */}
+            {/* <Route path="/train-details" element={<TrainDetails />} /> */}
+            <Route path="/passenger-details" element={<PassengerDetails />} />
+            {/* <Route path="/login" element={<Login />} /> */}
+            {/* <Route path="/register" element={<Register />} /> */}
+          </Routes>
+        </Box>
+        {/* <Footer /> */}
+      </BrowserRouter>
+    </Box>
+  );
 }
 
-export default App
+export default App;
+
