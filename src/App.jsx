f5653d242c63bf93b2bb60b6b009b964035e7330
dimalsha <madushaniagd@gmail.com>
2024-07-02 22:06:52 +0530

    create profile page and set route

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Register from "./pages/Register";
 import SearchBar from "./components/SearchBar";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
@@ -23,6 +24,8 @@ function App() {
             <Route path="/train-details" element={<TrainDetails />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={<Profile />} />
+
           </Routes>
         </Box>
         {/* <Footer /> */}
