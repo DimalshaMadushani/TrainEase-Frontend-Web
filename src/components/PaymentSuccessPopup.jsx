e4c07a08ecbb43ed37619d7ca03fe4035cd70968
dimalsha <madushaniagd@gmail.com>
2024-07-05 14:18:58 +0530

    Create PaymentSuccessPopup.jsx

---
+++ src/components/PaymentSuccessPopup.jsx
@@ -0,0 +1,33 @@
+//Pop up payment successful
+
+import React, { useState } from 'react';
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Typography, Box } from '@mui/material';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+
+const PaymentSuccessPopup = () => {
+  const [open, setOpen] = useState(true);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      <DialogContent>
+        <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+          <CheckCircleIcon color="success" style={{ fontSize: 80 }} />
+          <Typography variant="h6" style={{ marginTop: 16 }}>
+            Your payment was successful!
+          </Typography>
+        </Box>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color="primary">
+          Close
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default PaymentSuccessPopup;
